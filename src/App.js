a48d216ed8d2698300604be0279f2a5b70f599ee
PatrickLuguzza <luguzzapatrick@gmail.com>
2023-10-20 14:48:26 +0300

    updating app with shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import { Link, Route, Routes } from "react-router-dom";
-
-const HatsPage = (props) => {
-  return (
-    <div>
-      <h1>HATS</h1>
-    </div>
-  );
-};
-
-const MyPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <Link to="/hats">Hats</Link>
-
-      <h1>THIS PAGE IS NOT EMPTY</h1>
-    </div>
-  );
-};
+import { Route, Routes } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" Component={HomePage} />
-        <Route path="/hats" Component={HatsPage} />
-        <Route path="/page/pages" Component={MyPage} />
+        <Route path="/shop" Component={ShopPage} />
       </Routes>
     </div>
   );

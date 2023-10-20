df5fa4d325e92ab5c84ef756ce86aa80e2fd9814
PatrickLuguzza <luguzzapatrick@gmail.com>
2023-10-20 12:24:19 +0300

    Added navigator in the menu-item components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import { Link, Route, Routes } from "react-router-dom";
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>HATS</h1>
+    </div>
+  );
+};
+
+const MyPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Link to="/hats">Hats</Link>
+
+      <h1>THIS PAGE IS NOT EMPTY</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" Component={HomePage} />
+        <Route path="/hats" Component={HatsPage} />
+        <Route path="/page/pages" Component={MyPage} />
+      </Routes>
     </div>
   );
 }
